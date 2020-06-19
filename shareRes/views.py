737923e1a784975bbf48afb24e2946ae4ed37c06
djangojeng-e <headfat1218@gmail.com>
2020-06-19 10:35:41 +0900

    ShareRestaurant - templates files been rendered in views.

--- shareRes/views.py
+++ shareRes/views.py
@@ -4,17 +4,21 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Index')
+    # return HttpResponse('Index')
+    return render(request, 'shareRes/index.html')
 
 
 def restaurantDetail(request):
-    return HttpResponse('RestaurantDetail')
+    # return HttpResponse('RestaurantDetail')
+    return render(request, 'shareRes/restaurantDetail.html')
 
 
 def restaurantCreate(request):
-    return HttpResponse('restaurantCreate')
+    # return HttpResponse('restaurantCreate')
+    return render(request, 'shareRes/restaurantCreate.html')
 
 
 def categoryCreate(request):
-    return HttpResponse('categoryCreate')
+    # return HttpResponse('categoryCreate')
+    return render(request, 'shareRes/categoryCreate.html')
 
