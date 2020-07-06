86f96b9a1d8dbbda031a41e819bd8eadb8727580
djangojeng-e <headfat1218@gmail.com>
2020-07-06 17:36:02 +0900

    ShareRestaurant - sending email completed and tested fine.

--- sendEmail/views.py
+++ sendEmail/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse 
+from shareRes.models import * 
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 
 # Create your views here.
 
@@ -9,6 +15,30 @@ def sendEmail(request):
     inputReceiver = request.POST['inputReceiver']
     inputTitle = request.POST['inputTitle']
     inputContent = request.POST['inputContent']
-    print(checked_res_list, "/", inputReceiver, "/", inputTitle, "/", inputContent)
-    return HttpResponseRedirect(reverse('index'))
-    # return HttpResponse('sendEmail')
+
+    mail_html = "<html><body>"
+    mail_html = "<h1>맛집공유</h1>"
+    mail_html = "<p>" + inputContent + "<br>"
+    mail_html += "발신자님께서 공유하신 맛집은 다음과 같습니다.</p>"
+    for checked_res_id in checked_res_list:
+        restaurant = Restaurant.objects.get(id=checked_res_id)
+        mail_html += "<h2>" + restaurant.restaurant_name + "</h2>"
+        mail_html += "<h4> * 관련링크 </h4>" + "<p>" + restaurant.restaurant_link + "</p><br>"
+        mail_html += "<h4> * 상세내용 </h4>" + "<p>" + restaurant.restaurant_content + "</p><br>"
+        mail_html += "<h4> * 관련 키워드 </h4>" + "<p>" + restaurant.restaurant_keyword + "</p><br>"
+        mail_html += "<br>"
+        mail_html += "</body></html>"
+        print(mail_html)
+        # use smtp 
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login('headfat@gmail.com', 'yourpassword')
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = inputTitle
+        msg['From'] = "headfat@gmail.com"
+        msg['To'] = inputReceiver
+        mail_html = MIMEText(mail_html, 'html')
+        msg.attach(mail_html)
+        print(msg['To'], type(msg['To']))
+        server.sendmail(msg['From'], msg['To'].split(','), msg.as_string())
+        server.quit()
+        return HttpResponseRedirect(reverse('index'))     
