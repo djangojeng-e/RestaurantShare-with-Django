40183c41447c7f2a5975c2018f71aec4bd311eb3
djangojeng-e <headfat1218@gmail.com>
2020-07-06 19:48:15 +0900

    ShareRestaurant - sending email using django email

--- sendEmail/views.py
+++ sendEmail/views.py
@@ -5,6 +5,8 @@ from shareRes.models import *
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from django.core.mail import send_mail, EmailMessage
+from django.template.loader import render_to_string
 
 
 # Create your views here.
@@ -15,30 +17,51 @@ def sendEmail(request):
     inputReceiver = request.POST['inputReceiver']
     inputTitle = request.POST['inputTitle']
     inputContent = request.POST['inputContent']
-
-    mail_html = "<html><body>"
-    mail_html = "<h1>맛집공유</h1>"
-    mail_html = "<p>" + inputContent + "<br>"
-    mail_html += "발신자님께서 공유하신 맛집은 다음과 같습니다.</p>"
+    restaurants = []
     for checked_res_id in checked_res_list:
-        restaurant = Restaurant.objects.get(id=checked_res_id)
-        mail_html += "<h2>" + restaurant.restaurant_name + "</h2>"
-        mail_html += "<h4> * 관련링크 </h4>" + "<p>" + restaurant.restaurant_link + "</p><br>"
-        mail_html += "<h4> * 상세내용 </h4>" + "<p>" + restaurant.restaurant_content + "</p><br>"
-        mail_html += "<h4> * 관련 키워드 </h4>" + "<p>" + restaurant.restaurant_keyword + "</p><br>"
-        mail_html += "<br>"
-        mail_html += "</body></html>"
-        print(mail_html)
-        # use smtp 
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        server.login('headfat@gmail.com', 'yourpassword')
-        msg = MIMEMultipart('alternative')
-        msg['Subject'] = inputTitle
-        msg['From'] = "headfat@gmail.com"
-        msg['To'] = inputReceiver
-        mail_html = MIMEText(mail_html, 'html')
-        msg.attach(mail_html)
-        print(msg['To'], type(msg['To']))
-        server.sendmail(msg['From'], msg['To'].split(','), msg.as_string())
-        server.quit()
-        return HttpResponseRedirect(reverse('index'))     
+        restaurants.append(Restaurant.objects.get(id=checked_res_id))
+    
+    content = {'inputContent': inputContent, 'restaurants': restaurants}
+
+    msg_html = render_to_string('sendEmail/email_format.html', content)
+
+    msg = EmailMessage(subject = inputTitle, body=msg_html, from_email='headfat1218@gmail.com',
+                       bcc=inputReceiver.split(','))
+    msg.content_subtype = 'html'
+    msg.send()
+    return HttpResponseRedirect(reverse('index'))
+
+
+
+# def sendEmail(request):
+#     checked_res_list = request.POST.getlist('checks')
+#     inputReceiver = request.POST['inputReceiver']
+#     inputTitle = request.POST['inputTitle']
+#     inputContent = request.POST['inputContent']
+
+#     mail_html = "<html><body>"
+#     mail_html = "<h1>맛집공유</h1>"
+#     mail_html = "<p>" + inputContent + "<br>"
+#     mail_html += "발신자님께서 공유하신 맛집은 다음과 같습니다.</p>"
+#     for checked_res_id in checked_res_list:
+#         restaurant = Restaurant.objects.get(id=checked_res_id)
+#         mail_html += "<h2>" + restaurant.restaurant_name + "</h2>"
+#         mail_html += "<h4> * 관련링크 </h4>" + "<p>" + restaurant.restaurant_link + "</p><br>"
+#         mail_html += "<h4> * 상세내용 </h4>" + "<p>" + restaurant.restaurant_content + "</p><br>"
+#         mail_html += "<h4> * 관련 키워드 </h4>" + "<p>" + restaurant.restaurant_keyword + "</p><br>"
+#         mail_html += "<br>"
+#         mail_html += "</body></html>"
+#         print(mail_html)
+#         # use smtp 
+#         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+#         server.login('headfat@gmail.com', 'Yourpassword')
+#         msg = MIMEMultipart('alternative')
+#         msg['Subject'] = inputTitle
+#         msg['From'] = "headfat@gmail.com"
+#         msg['To'] = inputReceiver
+#         mail_html = MIMEText(mail_html, 'html')
+#         msg.attach(mail_html)
+#         print(msg['To'], type(msg['To']))
+#         server.sendmail(msg['From'], msg['To'].split(','), msg.as_string())
+#         server.quit()
+#         return HttpResponseRedirect(reverse('index'))     
